9720b6f5c6f3a3aa05993ac157cb930f83b6cc94
Gavin King <gavin.king@acando.no>
2022-01-17 12:29:37 +0100

    Add example of props from parent application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: FC<{ name?: string }> = ({ name = "<No name supplied>" }) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> {name} and save to reload.
         </p>
         <a
           className="App-link"
